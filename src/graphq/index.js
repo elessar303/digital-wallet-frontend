a79f3e900b56f8f37ee334b7a1365e193949a740
Junior Ayala <junior.ayala@weplan-latam.com>
2023-03-08 09:11:29 -0300

    update env variable

--- src/graphq/index.js
+++ src/graphq/index.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:3000/graphql',
+    uri: process.env.REACT_APP_API_ENDPOINT,
     cache: new InMemoryCache(),
 });
