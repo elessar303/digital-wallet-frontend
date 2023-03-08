f907e38512ce5ef274926f75d7584cc4346bae61
Junior Ayala <junior.ayala@weplan-latam.com>
2023-03-08 09:27:18 -0300

    update build

---
+++ build/precache-manifest.966cd19d4c4e106ace8286599c05ee3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d686c46cf0033e7ffbf2",
+    "url": "/static/css/main.8e4cd9c4.chunk.css"
+  },
+  {
+    "revision": "d686c46cf0033e7ffbf2",
+    "url": "/static/js/main.e47038ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a55503cbbda614ecdc05",
+    "url": "/static/js/2.a6e18cdd.chunk.js"
+  },
+  {
+    "revision": "6483934b9f1920f0d66c1860131f5aba",
+    "url": "/static/media/logo.6483934b.png"
+  },
+  {
+    "revision": "3d610f31033e27c67f06632f87dfcd8b",
+    "url": "/index.html"
+  }
+];
